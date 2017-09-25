16248dfda876cbcde8cb6b7a911c7507c6edc337
tejab6669 <tejab6669@HRH-IC0027339.wrdsb.ca>
2017-09-25 14:21:45 -0400

    Completed assignment 2

---
+++ src/Assignment2/Challenge.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment2;
+
+/**
+ *
+ * @author tejab6669
+ */
+public class Challenge {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
