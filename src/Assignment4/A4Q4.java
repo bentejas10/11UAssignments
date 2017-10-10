9315cdf051a469885bb287c34711cd3facc3f19b
tejab6669 <tejab6669@HRH-IC0027339.wrdsb.ca>
2017-10-10 13:18:34 -0400

    finished assignment 4 question 3

--- src/Assignment4/A4Q4.java
+++ src/Assignment4/A4Q4.java
@@ -4,6 +4,8 @@
  */
 package Assignment4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author tejab6669
@@ -14,6 +16,22 @@ public class A4Q4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("How much does the food for prom cost?");
+        Int outputFood = inputFood.nextInt();
+        Scanner inputFood = new Scanner(System.in);
+        
+        System.out.println("How much does the DJ cost?");
+        Scanner inputDj = new Scanner(System.in);
+        
+        System.out.println("How much does it cost to rent the hall?");
+        Scanner inputHall = new Scanner(System.in);
+        
+        System.out.println("How much does it cost for staff?");
+        Scanner inputStaff = new Scanner(System.in);
+        
+        System.out.println("How much for miscellaneous costs?");
+        Scanner inputMisc = new Scanner(System.in);
+        
         
     }
 }
