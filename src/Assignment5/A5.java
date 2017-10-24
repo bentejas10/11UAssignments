a18129a319754b3b0f5706b9e265735f2ebe560a
tejab6669 <tejab6669@HRH-IC0027339.wrdsb.ca>
2017-10-24 13:18:39 -0400

    started a5

---
+++ src/Assignment5/A5.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment5;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author tejab6669
+ */
+public class A5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       // create a scanner
+        Scanner input = new Scanner(System.in);
+        
+        //ask for a word to translate
+        System.out.println("Please enter a word.");
+        String word = input.nextLine();
+        
+        StringBuilder =  
+    }
+}
