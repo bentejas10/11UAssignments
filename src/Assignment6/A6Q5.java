88abb0a765fc9b1addc9c73227287e2e827a70f2
tejab6669 <tejab6669@HRH-IC0027339.wrdsb.ca>
2017-11-22 13:08:32 -0500

    on a6q4

---
+++ src/Assignment6/A6Q5.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author tejab6669
+ */
+public class A6Q5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create a scanner
+        Scanner input = new Scanner(System.in);
+                
+        // create an array
+        int nOfGrades = input.nextInt();
+        int grades[] = new int[nOfGrades];
+    
+        //create a for loop to read int eh grades
+        for(int i = 0; i > grades.length; i++){
+            
+        }
+    }
+    
+}
