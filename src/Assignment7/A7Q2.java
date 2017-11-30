cb7dbd97cc763147138c2e90beb607c3b6bc93c7
tejab6669 <tejab6669@HRH-IC0027339.wrdsb.ca>
2017-11-30 13:18:28 -0500

    on a7q3

--- src/Assignment7/A7Q2.java
+++ src/Assignment7/A7Q2.java
@@ -12,13 +12,26 @@ import java.util.Scanner;
  */
 public class A7Q2 {
 
-     public void sayGradeL(String grade){
-    
+     public void examGrade(int grade){
+    if(grade >= 80){
+        System.out.println("Your grade is an B");
+    }else if(grade >= 70 && grade < 80){
+        System.out.println("Your grade is a B");
+    }else if(grade >= 60 && grade < 70){
+        System.out.println("Your grade is a C");
+    }else if(grade >= 50 && grade <60){
+        System.out.println("Your grade is a D");
+    }else if(grade < 50 && grade >= 0){
+        System.out.println("Your grade is an F");
+    }
      }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // create an object for method
+        A7Q2 test = new A7Q2();
+        
         // create a scanner
         Scanner input = new Scanner(System.in);
         
@@ -26,6 +39,7 @@ public class A7Q2 {
         System.out.println("What is your grade?");
         int grade = input.nextInt();
         
-        
+        //output
+        test.examGrade(grade);
     }
 }
---
+++ src/Assignment7/A7Q3.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment7;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author tejab6669
+ */
+public class A7Q3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create a scanner
+        Scanner input = new Scanner(System.in);
+        
+        
+    }
+}
