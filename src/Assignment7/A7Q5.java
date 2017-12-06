465f2be510eeb05aa345daef4bd452a6ceda7628
tejab6669 <tejab6669@HRH-IC0027339.wrdsb.ca>
2017-12-06 13:10:12 -0500

    on a7q6

--- src/Assignment7/A7Q5.java
+++ src/Assignment7/A7Q5.java
@@ -4,16 +4,47 @@
  */
 package Assignment7;
 
+import java.util.Scanner;
+
 /**
  *
  * @author tejab6669
  */
 public class A7Q5 {
 
+    public void chaotic(int nOfLines) {
+        // to apply number of lines wanted
+        for (int i = 0; i < nOfLines; i++) {
+            // to generate a random number
+            int randNum = (int) (Math.random() * (5 - 1 + 1)) + 1;
+            // to create asterisks equal to the random number generated
+            for (int u = 0; u < randNum; u++) {
+                // print the asterisks
+                System.out.print("*");
+            }
+            // go to the next line
+            System.out.println(" ");
+        }
+
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // create object to run methods
+        A7Q5 test = new A7Q5();
+
+        // create a scannner
+        Scanner input = new Scanner(System.in);
+
+        // ask the user for a number
+        System.out.println("Please enter a number: ");
+
+        // for the user to input number
+        int nOfLines = input.nextInt();
+
+        // output
+        test.chaotic(nOfLines);
     }
 }
