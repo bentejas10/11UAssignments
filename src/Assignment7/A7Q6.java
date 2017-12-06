f66c80f20df7c87d63b304767104c8a5fa04a0bb
tejab6669 <tejab6669@HRH-IC0027339.wrdsb.ca>
2017-12-06 13:18:48 -0500

    on a7q6

---
+++ src/Assignment7/A7Q6.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment7;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author tejab6669
+ */
+public class A7Q6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in); 
+        
+        
+    }
+}
