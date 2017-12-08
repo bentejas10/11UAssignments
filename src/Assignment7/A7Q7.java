27fd1756e4b3eebe9f32782efb18ffe2cb63efd1
tejab6669 <tejab6669@HRH-IC0027339.wrdsb.ca>
2017-12-08 13:12:38 -0500

    a7q8

--- src/Assignment7/A7Q7.java
+++ src/Assignment7/A7Q7.java
@@ -12,6 +12,34 @@ import java.util.Scanner;
  */
 public class A7Q7 {
 
+    public int firstDigit(int number) {
+        // if statement to convert negative number to positive
+        if (number < 0) {
+
+            // create an integer where the negative inputted number is positive
+            int posNum = number * -1;
+
+            // while loop to divide number down until it gets to one digit
+            while (number > 10) {
+                number = number / 10;
+            }
+
+            // return final outcome
+            return number;
+
+        } else {
+
+            // while loop to divide number down until it gets to one digit
+            while (number > 10) {
+                number = number / 10;
+
+            }
+            
+            // return final output
+            return number;
+        }
+    }
+
     /**
      * @param args the command line arguments
      */
@@ -25,12 +53,11 @@ public class A7Q7 {
         // ask for number
         System.out.println("Please enter your number: ");
         int number = input.nextInt();
-        int remainder = 0;
-        while(number >= 10){
-            remainder = number / 10;
-            
-        }
-        
-        System.out.println(remainder);
+
+        // create integer to hold the first digit
+        int firstDig = test.firstDigit(number);
+
+        // tell user the first digit
+        System.out.println("The first digit of the number " + number + " is " + firstDig);
     }
 }
---
+++ src/Assignment7/A7Q8.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment7;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author tejab6669
+ */
+public class A7Q8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create an object to run method
+        A7Q8 test = new A7Q8();
+        
+        // create a scanner
+        Scanner input = new Scanner(System.in);
+        
+        // ask user for number
+        System.out.println("Please enter a number: ");
+        int number = input.nextInt();
+       
+        
+    }
+}
