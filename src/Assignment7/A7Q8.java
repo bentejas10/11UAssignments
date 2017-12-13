9a102436cf3e368f3c276db08763cca39666971f
tejab6669 <tejab6669@HRH-IC0027339.wrdsb.ca>
2017-12-13 14:22:38 -0500

    a7q8

--- src/Assignment7/A7Q8.java
+++ src/Assignment7/A7Q8.java
@@ -26,6 +26,48 @@ public class A7Q8 {
         System.out.println("Please enter a number: ");
         int number = input.nextInt();
        
+        // create variable to change the valu ewithout changing inital number
+        int forNum = number;
+        
+        // create a variable to keep track of the number lengths
+        int tracker = 1;
+        
+        while(forNum >= 10){
+            forNum = forNum / 10;
+            tracker = tracker + 1;
+        }
+        
+        // create variable for last digit in number
+        int lastDig = number % 10;
+        int evenOrOdd = lastDig % 2;
+        // for loop to go through each numerical value and see if it is even or odd
+        if(evenOrOdd == 0){
+            System.out.println("False");
+        }else{
+            
+            // create int to hold number value
+            int FLNumber = number;
+            
+        for(int i = 0; i < tracker; i++){
+            
+            FLNumber = FLNumber / 10;
+            int last = FLNumber % 10;
+            int EO = last % 2;
+            if(EO == 0){
+                System.out.println("False");
+                break;
+            }
+            System.out.println("True");
+            break;
+            }
+            
+        }
+        
+        
+        }
+        
         
     }
-}
+        
+
+
